7bf6d324d584014dc4c5cc51a56a74671e9f0d7b
Jv Empleo <empleojv28@gmail.com>
2024-06-11 14:42:04 +0800

    commit

--- src/app/maintenance/maintenance.component.ts
+++ src/app/maintenance/maintenance.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from "@angular/core";
 
 @Component({
-  selector: 'app-maintenance',
-  templateUrl: './maintenance.component.html',
-  styleUrls: ['./maintenance.component.scss']
+    selector: "app-maintenance",
+    templateUrl: "./maintenance.component.html",
+    styleUrls: ["./maintenance.component.scss"],
 })
 export class MaintenanceComponent {
+    constructor(private el: ElementRef) {}
 
+    scrollToSection(sectionId: string) {
+        const element = this.el.nativeElement.querySelector(`#${sectionId}`);
+        if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+        }
+    }
 }
