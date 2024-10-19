6e0868dbe30824f7be11817adab06629fada6264
Alli <alliyah@gmail.com>
2024-10-19 14:55:52 +0800

    added page-load comp. and load-page service, responsive in maintenance?

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -1,4 +1,6 @@
 export const config = {
-    apiUrl: 'https://clockgeo.geoplanph.com/api',
-    apiUrl1: 'https://clockgeo.geoplanph.com',
+    // apiUrl: 'https://clockgeo.geoplanph.com/api',
+    // apiUrl1: 'https://clockgeo.geoplanph.com',
+    apiUrl: 'http://localhost:3000/api',
+    apiUrl1: 'http://localhost:4200',
 };
